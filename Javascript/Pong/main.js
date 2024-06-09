50e067db6f59e42e5ff2590372b49f06fec67026
fgalan-r <102490188+tremerin@users.noreply.github.com>
2024-06-09 17:39:12 +0200

    Add files via upload

--- Javascript/Pong/main.js
+++ Javascript/Pong/main.js
@@ -3,13 +3,16 @@ const canvas = document.getElementById("canvasGame");
 const context = canvas.getContext("2d");
 canvas.height = 400;
 canvas.width = 640;
-
 //Marcadores
 let scoreOne = 0;
 let scoreTwo = 0;
-
 //Game stats
 let speedGame = 5;
+//Inputs, 0 = keyup, 1 = keydown
+let player1Up = 0; 
+let player1Down = 0;
+let player2Up = 0;
+let player2Down = 0;
 
 //Palas y pelota
 class Element{
@@ -52,6 +55,15 @@ function    drawElements()
     drawElement(ball);
     displayScore();
 }
+function    moveElements()
+{
+    let move = player1Up - player1Down;
+    if (move == 1) playerOne.y -= playerOne.speed;
+    else if (move == -1) playerOne.y += playerOne.speed;
+    move = player2Up - player2Down;
+    if (move == 1) playerTwo.y -= playerTwo.speed;
+    else if (move == -1) playerTwo.y += playerTwo.speed;
+}
 //Ball bounce
 function ballBounce()
 {
@@ -63,20 +75,77 @@ function ballCollision()
 
 }
 /*  KEYS            */
+window.addEventListener("keydown", keyPressDown); //keypress, keydown
+function    keyPressDown(e)
+{
+    if (e.key == "w") {
+        player1Up = 1;
+        console.log("w down");
+    }
+    if (e.key == "s") {
+        player1Down = 1;
+        console.log("s down");
+    }
+    if (e.key == "o") {
+        player2Up = 1;
+        console.log("o down");
+    }
+    if (e.key == "l") {
+        player2Down = 1;
+        console.log("l down");
+    }
+}
+window.addEventListener("keyup", keyPressUp); //keypress, keydown
+function    keyPressUp(e)
+{
+    if (e.key == "w") {
+        player1Up = 0;
+        console.log("w up");
+    }
+    if (e.key == "s") {
+        player1Down = 0;
+        console.log("s up");
+    }
+    if (e.key == "o") {
+        player2Up = 0;
+        console.log("o up");
+    }
+    if (e.key == "l") {
+        player2Down = 0;
+        console.log("l up");
+    }
+}
 //error con dos pulsaciones simultaneas
-window.addEventListener("keydown", keyPress, false); //keypress, keydown
-function    keyPress(e)
+/* window.addEventListener("keypress", keyPress1); //keypress, keydown
+function    keyPress1(e)
 {
     //player 1
-    if      (e.key == "w") playerOne.y -= playerOne.speed;
-    else if (e.key == "s") playerOne.y += playerOne.speed;
-    //player 2
-    if      (e.key == "o") playerTwo.y -= playerTwo.speed;
-    else if (e.key == "l") playerTwo.y += playerTwo.speed;
+    if      (e.key == "w") {
+        playerOne.y -= playerOne.speed;
+        console.log("w");
+    }
+    else if (e.key == "s") {
+        playerOne.y += playerOne.speed;
+        console.log("s");
+    }
 }
+window.addEventListener("keypress", keyPress2); //keypress, keydown
+function    keyPress2(e)
+{
+    //player 2
+    if      (e.key == "o") {
+        playerTwo.y -= playerTwo.speed;
+        console.log("o");
+    }    
+    else if (e.key == "l") {
+        playerTwo.y += playerTwo.speed;
+        console.log("l");
+    }
+} */
 /*  GAME LOOP       */
 function    loop()
 {
+    moveElements();
     drawElements();
     window.requestAnimationFrame(loop);
 }
