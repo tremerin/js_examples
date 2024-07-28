3117bfa34a2bee733f6785e3302434a73dddc2cb
Tremerin <tremerin@hotmail.com>
2024-07-28 13:27:32 +0200

    pongModel

--- Javascript/Bootstrap/responsiveCanvas.js
+++ Javascript/Bootstrap/responsiveCanvas.js
@@ -3,10 +3,34 @@ const canvas = document.getElementById("tournamet");
 const context = canvas.getContext("2d");
 const canvasContainer = document.getElementById('canvasContainer');
 
-function    setCanvas(height, width) {
-    canvas.height = height;
-    canvas.width = width;
+//pong model
+function scale(size, div, mul) {
+   return (size/div) * mul;
 }
+
+class PongModel {
+    constructor() {
+        //field
+        this.fieldWidth = 1;
+        this.fieldHeight = scale(this.fieldWidth, 4, 3);
+        this.fieldMarginX = scale(this.fieldWidth, 100, 1);
+        this.fieldMarginY = scale(this.fieldHeight, 100, 1);
+        //paddle
+        this.paddleWidth = scale(this.fieldWidth, 100, 1);
+        this.paddleHeight = scale(this.fieldHeight, 100, 5);
+        this.paddleInitY = (this.fieldHeight / 2) - (this.paddelHeight / 2);
+        this.paddleTopLimit = this.fieldMarginY;
+        this.paddleBottonLimit = this.fieldHeight - this.fieldMarginY - this.paddleHeight;
+        this.paddleOnePosX = this.fieldMarginX;
+        this.paddleTwoPosX = this.fieldWidth - this.fieldMarginX - this.paddleWidth;
+        //ball
+        this.ballRadius = scale(this.fieldWidth, 100, 1);
+        this.ballInitX = (this.fieldWidth / 2) - (this.ballRadius / 2);
+        this.ballInitY = (this.fieldHeight / 2) - (this.ballRadius / 2);
+    }
+}
+
+const pong = new PongModel();
 
 //scale
 let xScale = 4;
@@ -16,6 +40,11 @@ let yScale = 3;
 let xSize = 0;
 let ySize = 0;
 
+function    setCanvas(height, width) {
+    canvas.height = height;
+    canvas.width = width;
+}
+
 function    resizeCanvas(xScale, yScale) {
     let rect = canvasContainer.getBoundingClientRect();
     xSize = rect.width;
@@ -23,27 +52,38 @@ function    resizeCanvas(xScale, yScale) {
     setCanvas(ySize, xSize); 
 }
 
+//draw pong
+function drawPong() {
+
+}
+
 //margin %100, number of squares
-function    draw(squares, margin) {
+function drawTest(squares, margin) {
     let initX = ((xSize/100) * margin) / 2;
     let initY = ((ySize/100) * margin) / 2;
-    let lenX = xSize - (((xSize/100) * margin) / 2);
-    let lenY = ySize - (((ySize/100) * margin) / 2);
-    let squareX = lenX / squares;
-    let squareY = lenY / squares;
-    console.log(squareX, squareY);
+    let squareX = (xSize - ((xSize/100) * margin)) / squares;
+    let squareY = (ySize - ((ySize/100) * margin)) / squares;
     context.fillStyle = "#ff0";
     for (let i = 0; i < squares; i++)
     {
         for (let j = 0; j < squares; j++)
         {
-            if (j % 2 == 0 || i % 2 == 0)
+            if (j % 2 == 0)
             {
-                context.fillRect( initX + (j * squareX)
-                                , initY + (i * squareY)
-                                , (initX + (j * squareX)) + squareX
-                                , (initY + (i * squareY)) + squareY);
-            }    
+                if (i % 2 != 0)
+                    context.fillRect( initX + (j * squareX)
+                                    , initY + (i * squareY)
+                                    , squareX
+                                    , squareY);
+            }
+            else 
+            {
+                if (i % 2 == 0)
+                    context.fillRect( initX + (j * squareX)
+                                    , initY + (i * squareY)
+                                    , squareX
+                                    , squareY);
+            }   
         }
     }
 }
@@ -51,18 +91,19 @@ function    draw(squares, margin) {
 //setCanvas(300, 400); //por defecto 4:3
 
 // loop
-let lastTime = 0;
+let lastTime = 0; 
+let lastSize = 0; //control scale change 
 let fps = 0;
 function    loop(time)
 {
     fps++;
     if (time - lastTime >= 1000) // ha pasado un segundo
     {
-        console.log(fps);
+        //console.log(fps);
         fps = 0;
         lastTime = time;
         resizeCanvas(xScale, yScale);
-        draw(8, 40)
+        drawTest(10, 1)
     }
     window.requestAnimationFrame(loop);
 }
